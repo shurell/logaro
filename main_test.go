e3103cb418f3272a20122556f38846c85e483ef6
Shurell <0788746@gmail.com>
2024-03-15 16:47:24 +0300

    Еще немого тестов

--- main_test.go
+++ main_test.go
@@ -12,7 +12,28 @@ func TestFaviconHandler(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Ожидаемый результат OK, получено: %d", resp.StatusCode)
+	}
+}
 
+func TestJsHandler(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(jsHandler))
+	resp, err := http.Get(server.URL)
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Ожидаемый результат OK, получено: %d", resp.StatusCode)
+	}
+}
+
+func TestCssHandler(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(cssHandler))
+	resp, err := http.Get(server.URL)
+	if err != nil {
+		t.Error(err)
+	}
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("Ожидаемый результат OK, получено: %d", resp.StatusCode)
 	}
